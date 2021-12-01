4e8791f5001efedbd8be43b8ddeb62ca8eebc614
Salman Niazi <salman@logicalclocks.com>
2021-12-01 21:53:25 +0100

    added java test

---
+++ toy_ndb_wrapper/jtest/src/main/java/com/lc/test/Main.java
@@ -0,0 +1,77 @@
+package com.lc.test;
+
+import com.mysql.clusterj.ClusterJException;
+import com.mysql.clusterj.ClusterJHelper;
+import com.mysql.clusterj.Session;
+import com.mysql.clusterj.SessionFactory;
+
+import java.util.Properties;
+
+public class Main {
+  public SessionFactory sessionFactory;
+
+  public static void main(String argv[]) throws Exception {
+    new Main().test();
+  }
+
+  public void test() throws Exception {
+    setUpDBConnection();
+
+    long startTime = System.currentTimeMillis();
+
+    int iterations = 10000;
+    for (int i = 0; i < iterations; i++) {
+      writeRow(i, i);
+    }
+
+    for (int i = 0; i < iterations; i++) {
+      findRow( i);
+    }
+
+    long endTime = System.currentTimeMillis();
+    System.out.println("Time taken: "+(endTime - startTime)+" ms");
+  }
+
+  public void writeRow(int key, int value) {
+    Session session = sessionFactory.getSession();
+    TestTable.TestTableDTO persistable = session.newInstance(TestTable.TestTableDTO.class);
+    persistable.setAttr1(key);
+    persistable.setAttr2(value);
+    session.savePersistent(persistable);
+    session.release(persistable);
+    session.close();
+  }
+
+  public void findRow(int key) {
+    Session session = sessionFactory.getSession();
+    TestTable.TestTableDTO lTable = (TestTable.TestTableDTO) session.find(TestTable.TestTableDTO.class,
+      key);
+    if (lTable.getAttr2() < 0) {
+      System.err.println("Wrong value read for index " + key);
+    }
+    session.release(lTable);
+    session.close();
+  }
+
+  public void setUpDBConnection() throws Exception {
+    Properties props = new Properties();
+    props.setProperty("com.mysql.clusterj.connectstring", "localhost");
+    props.setProperty("com.mysql.clusterj.database", "test_db");
+    props.setProperty("com.mysql.clusterj.connect.retries", "4");
+    props.setProperty("com.mysql.clusterj.connect.delay", "5");
+    props.setProperty("com.mysql.clusterj.connect.verbose", "1");
+    props.setProperty("com.mysql.clusterj.connect.timeout.before", "60");
+    props.setProperty("com.mysql.clusterj.connect.timeout.after", "5");
+    props.setProperty("com.mysql.clusterj.max.transactions", "1024");
+    props.setProperty("com.mysql.clusterj.connection.pool.size", "1");
+    props.setProperty("com.mysql.clusterj.max.cached.instances", "256");
+    props.setProperty("com.mysql.clusterj.connection.reconnect.timeout", "5");
+
+    try {
+      sessionFactory = ClusterJHelper.getSessionFactory(props);
+    } catch (ClusterJException ex) {
+      throw ex;
+    }
+    System.out.println("Connected");
+  }
+}
---
+++ toy_ndb_wrapper/jtest/src/main/java/com/lc/test/TestTable.java
@@ -0,0 +1,26 @@
+package com.lc.test;
+
+import com.mysql.clusterj.annotation.Column;
+import com.mysql.clusterj.annotation.PersistenceCapable;
+import com.mysql.clusterj.annotation.PrimaryKey;
+
+public class TestTable {
+
+  public static final String TABLE_NAME = "test_table";
+  public static final String ATTR1 = "ATTR1";
+  public static final String ATTR2 = "ATTR2";
+
+  @PersistenceCapable(table = TABLE_NAME)
+  public interface TestTableDTO {
+
+    @PrimaryKey
+    @Column(name = ATTR1)
+    int getAttr1();
+    void setAttr1(int key);
+
+    @Column(name = ATTR2)
+    int getAttr2();
+    void setAttr2(int val);
+
+  }
+}
